6f154a93a3ec9bb858b4f8bf391b79ff504519d2
gsiri007 <216904534+gsiri007@users.noreply.github.com>
2026-03-12 21:24:21 +0530

    book cards rendering

--- readABook/script.js
+++ readABook/script.js
@@ -1,7 +1,11 @@
 const search = document.getElementById('search-bar');
+const bookCardContainer = document.getElementById('book-card-container');
+const stamp = document.getElementById('stamp')
 
 document.getElementById('search-btn').addEventListener('click', async () => {
   const searchValue = search.value;
+  search.value = "";
+  stamp.classList.add('disabled');
 
   if (!searchValue) {
     return;
@@ -28,14 +32,33 @@ document.getElementById('search-btn').addEventListener('click', async () => {
 
   const books = data.docs;
 
+  let bookCards = [];
+
   for (let i = 0; i < 10 && i < books.length; ++i) {
-    const html = `
-      <img src="https://covers.openlibrary.org/b/ID/${books[i].cover_i}-M.jpg" />
-    `
-    console.log(html);
+
+    let coverID = books[i]?.cover_i;
+    let coverSrc = '';
+
+    if (coverID) {
+      coverSrc = `https://covers.openlibrary.org/b/ID/${coverID}-M.jpg`;
+    } else {
+      coverSrc = './img/book.png';
+    }
+
+    const bookCard = `
+      <div id="book-card" class="book-card">
+        <img class="book-cover"
+            src="${coverSrc}"
+        alt="">
+        <div class="book-info">
+          <h2 class="name">${books[i]?.title}</h2>
+          <p class="author">${books[i]?.author_name[0]}</p>
+          <p class="year">${books[i]?.first_publish_year}</p>
+        </div>
+      </div>`;
+
+    bookCards.push(bookCard);
   }
 
-
-  search.value = "";
-  
+  bookCardContainer.innerHTML = bookCards.join('\n');
 });
